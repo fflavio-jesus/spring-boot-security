c7cd74d37acc2899ff86f2bba55efec2c8bfd4dd
Jesus <jfcjesus@rd.com.br>
2023-03-14 21:09:49 -0300

    initial commit

---
+++ src/main/java/jesus/springbootsegurity/SpringBootSegurityApplication.java
@@ -0,0 +1,13 @@
+package jesus.springbootsegurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSegurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSegurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jesus/springbootsegurity/SpringBootSegurityApplicationTests.java
@@ -0,0 +1,13 @@
+package jesus.springbootsegurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSegurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
